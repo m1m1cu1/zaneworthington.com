7d64d51f4e1ab8ce4493acabebe479034c2c89cd
Zane  Worthington <cui@Zanes-MacBook.local>
2026-03-31 21:11:10 -0700

    style refresh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
-  integrations: [react()]
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  integrations: [react()],
 });
